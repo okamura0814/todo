71f35912978566eeba771ba317f6373c37ee6289
okapi0814 <okapi300212@gmail.com>
2022-10-03 22:33:21 +0900

    ユーザー登録画面追加

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,11 +49,33 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+        $rulus = [
+            'nickname' => ['required', 'string', 'max:100'],
+            'name' => ['required', 'string', 'max:100'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+            'password' => ['required', 'string', 'min:8', 'max:32', 'confirmed', 'regex:/(?=.*[a-zA-Z])(?=.*[0-9])/'],
+          ];
+        
+        $message = [
+            'nickname.required' => 'ユーザーIDを入力してください',
+            'nickname.string' => '文字列で入力してください',
+            'nickname.max' => '100文字以内で入力してください',
+            'name.required' => '名前を入力してください',
+            'name.string' => '文字列で入力してください',
+            'name.max' => '100文字以内で入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.string' => '文字列で入力してください',
+            'email.email' => 'メールアドレスをご確認ください',
+            'email.max' => '255文字以内で入力してください',
+            'email.unique' => 'このメールアドレスは既に使用されています',
+            'password.required' => 'パスワードを入力してください',
+            'password.string' => '文字列で入力してください',
+            'password.min' => '8文字以上で入力してください',
+            'password.max' => '32文字以内で入力してください',
+            'password.confirmed' => 'パスワード確認が一致しません',
+            'password.regex' => '半角数字と半角英字のどちらも含むパスワードを入力してください',
+        ];
+        return Validator::make($data, $rulus, $message);
     }
 
     /**
@@ -65,6 +87,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'nickname' => $data['nickname'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,12 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">メール認証</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                        メール認証リンクがあなたのメールアドレス宛に送信されました。
                         </div>
                     @endif
 
