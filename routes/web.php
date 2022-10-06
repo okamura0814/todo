12b27210c2e4118559a7bbbec4908fd0545c7c65
okapi0814 <okapi300212@gmail.com>
2022-10-06 22:42:28 +0900

    ログイン画面の日本語対応

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes(['verify' => true]);
@@ -26,12 +26,9 @@ Route::get('/verify', function () {
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
-
     return redirect('/home');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::get('/profile', function () {
     // 確認済みのユーザーのみがこのルートにアクセス可能
 })->middleware('verified');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
